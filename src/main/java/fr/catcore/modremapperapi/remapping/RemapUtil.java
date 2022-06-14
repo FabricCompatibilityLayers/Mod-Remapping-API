b105ffda266e228efc3991a36379f3fa32f6cbf2
Caroline Joy Bell <carolinejoybell@gmail.com>
2022-06-14 10:56:55 -0700

    don't try to download libs if URL is empty

--- src/main/java/fr/catcore/modremapperapi/remapping/RemapUtil.java
+++ src/main/java/fr/catcore/modremapperapi/remapping/RemapUtil.java
@@ -45,7 +45,7 @@ public class RemapUtil {
                 for (RemapLibrary library : remapper.getRemapLibraries()) {
                     File libPath = new File(Constants.LIB_FOLDER, library.fileName);
 
-                    if (!libPath.exists()) {
+                    if (!libPath.exists() && !library.url.isEmpty()) {
                         try (BufferedInputStream inputStream = new BufferedInputStream(new URL(library.url).openStream())) {
                             try (BufferedOutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(libPath.toPath()))) {
                                 byte[] buffer = new byte[2048];
