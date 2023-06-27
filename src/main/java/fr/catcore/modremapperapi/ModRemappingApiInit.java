5b3b25ca19a7d86f2d4e96820ef47c13ed8e9949
Cat Core <arthur.koalas@free.fr>
2023-06-27 15:45:35 +0200

    Switch to spASM and stop using FabricASM

--- src/main/java/fr/catcore/modremapperapi/ModRemappingApiInit.java
+++ src/main/java/fr/catcore/modremapperapi/ModRemappingApiInit.java
@@ -3,6 +3,7 @@ package fr.catcore.modremapperapi;
 import static fr.catcore.modremapperapi.ModRemappingAPI.init;
 
 public class ModRemappingApiInit implements Runnable {
+    public ModRemappingApiInit() {}
     @Override
     public void run() {
         init();
