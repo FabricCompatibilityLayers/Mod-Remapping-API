f38ae248d872d67c14c790144b3e9f362df29346
Cat Core <arthur.koalas@free.fr>
2025-03-12 23:17:03 +0100

    More refactoring

--- src/main/java/io/github/fabriccompatibiltylayers/modremappingapi/impl/ModDiscoverer.java
+++ src/main/java/io/github/fabriccompatibiltylayers/modremappingapi/impl/ModDiscoverer.java
@@ -8,17 +8,16 @@ import io.github.fabriccompatibiltylayers.modremappingapi.impl.utils.CacheUtils;
 import io.github.fabriccompatibiltylayers.modremappingapi.impl.utils.FileUtils;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.impl.launch.FabricLauncherBase;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.include.com.google.common.collect.ImmutableList;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class ModDiscoverer {
     private static final Map<String, List<String>> EXCLUDED = new HashMap<>();
@@ -44,8 +43,8 @@ public class ModDiscoverer {
                     if (!Files.exists(cacheFolder)) Files.createDirectories(cacheFolder);
                     else io.github.fabriccompatibiltylayers.modremappingapi.impl.utils.FileUtils.emptyDir(cacheFolder);
 
-                    mods.addAll(discoverModsInFolder(mcSubFolder.toFile(), cacheFolder.toFile()));
-                } catch (IOException e) {
+                    mods.addAll(discoverModsInFolder(mcSubFolder, cacheFolder));
+                } catch (IOException | URISyntaxException e) {
                     e.printStackTrace();
                 }
             }
@@ -97,7 +96,7 @@ public class ModDiscoverer {
         for (Map.Entry<Path, Path> entry : convertMap.entrySet()) {
             try {
                 if (Files.isDirectory(entry.getKey())) {
-                    io.github.fabriccompatibiltylayers.modremappingapi.impl.utils.FileUtils.zipFolder(entry.getKey(), entry.getValue());
+                    FileUtils.zipFolder(entry.getKey(), entry.getValue());
                 } else {
                     Files.copy(entry.getKey(), entry.getValue(), StandardCopyOption.REPLACE_EXISTING);
                 }
@@ -114,97 +113,90 @@ public class ModDiscoverer {
         return map;
     }
 
-    private static List<ModEntry> discoverModsInFolder(File folder, File destination) {
+    private static Optional<ModEntry> discoverFolderMod(Path folder, Path destinationFolder) throws IOException {
+        String name = folder.getFileName().toString().replace(" ", "_");
+        Path destination = destinationFolder.resolve(name + ".zip");
+
+        final boolean[] hasClasses = {false};
+
+        Files.walkFileTree(folder, new SimpleFileVisitor<Path>() {
+            @Override
+            public @NotNull FileVisitResult visitFile(Path file, @NotNull BasicFileAttributes attrs) throws IOException {
+                if (file.toString().endsWith(".class")) {
+                    hasClasses[0] = true;
+                    return FileVisitResult.TERMINATE;
+                }
+
+                return super.visitFile(file, attrs);
+            }
+        });
+
+        if (hasClasses[0]) {
+            return Optional.of(
+                    new DefaultModEntry(
+                            name,
+                            folder.toFile(),
+                            destination.toFile()
+                    )
+            );
+        }
+
+        return Optional.empty();
+    }
+
+    private static Optional<ModEntry> discoverFileMod(Path file, Path destinationFolder) throws IOException {
+        String fileName = file.getFileName().toString().replace(" ", "_");
+        String modName = fileName.replace(".jar", "").replace(".zip", "");
+
+        List<String> entries = FileUtils.listZipContent(file);
+
+        for (String entry : entries) {
+            if (Objects.equals(entry, "/fabric.mod.json")) break;
+
+            if (entry.endsWith(".class")) {
+                return Optional.of(
+                        new DefaultModEntry(
+                                modName,
+                                file.toFile(),
+                                destinationFolder.resolve(fileName).toFile()
+                        )
+                );
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private static List<ModEntry> discoverModsInFolder(Path folder, Path destination) throws IOException, URISyntaxException {
         List<ModEntry> mods = new ArrayList<>();
 
-        File[] folderFiles = folder.listFiles();
-        if (!folder.isDirectory() || folderFiles == null) return ImmutableList.of();
+        if (!Files.isDirectory(folder)) return ImmutableList.of();
 
-        for (File file : folderFiles) {
-            String name = file.getName().replace(" ", "_");
-            if (file.isDirectory() || (file.isFile() && (name.endsWith(".jar") || name.endsWith(".zip")))) {
-                File remappedFile = new File(destination, name);
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(folder)) {
+            for (Path path : stream) {
+                String name = path.getFileName().toString();
 
-                List<ModEntry> modName = new ArrayList<>();
+                if (Files.isDirectory(path)) {
+                    discoverFolderMod(path, destination)
+                            .ifPresent(mods::add);
+                } else if (Files.exists(path) && (name.endsWith(".jar") || name.endsWith(".zip"))) {
+                    discoverFileMod(path, destination)
+                            .ifPresent(mods::add);
+                }
+            }
+        }
 
-                boolean hasClass = false;
-                boolean fabric = false;
-
-                File[] fileFiles = file.listFiles();
-
-                if (file.isDirectory() && fileFiles != null) {
-                    remappedFile = new File(destination, name + ".zip");
-                    for (File subFile : fileFiles) {
-                        String subName = subFile.getName();
-                        if (subFile.isFile()) {
-                            if (subName.endsWith(".class")) {
-                                hasClass = true;
-                            }
-                        }
-                    }
-
-                    if (/* modName.isEmpty() && */ hasClass) {
-                        modName.add(new DefaultModEntry(
-                                name.replace(".zip", "").replace(".jar", ""),
-                                remappedFile,
-                                file
-                        ));
-                    }
-
-                    if (!modName.isEmpty() && EXCLUDED.containsKey(modName.get(0).modName)) {
-                        for (String excluded :
-                                EXCLUDED.get(modName.get(0).modName)) {
-                            File excludedFile = new File(file, excluded);
-                            if (excludedFile.delete()) {
-                                Constants.MAIN_LOGGER.debug("File deleted: " + excludedFile.getName());
-                            }
+        for (ModEntry modEntry : mods) {
+            if (EXCLUDED.containsKey(modEntry.modId)) {
+                if (Files.isDirectory(modEntry.file.toPath())) {
+                    for (String excluded : EXCLUDED.get(modEntry.modId)) {
+                        if (Files.deleteIfExists(modEntry.file.toPath().resolve(excluded))) {
+                            Constants.MAIN_LOGGER.debug("File deleted: " + modEntry.file.toPath().resolve(excluded));
                         }
                     }
                 } else {
-                    try {
-                        FileInputStream fileinputstream = new FileInputStream(file);
-                        ZipInputStream zipinputstream = new ZipInputStream(fileinputstream);
-                        while (true) {
-                            ZipEntry zipentry = zipinputstream.getNextEntry();
-                            if (zipentry == null) {
-                                zipinputstream.close();
-                                fileinputstream.close();
-                                break;
-                            }
-
-                            String s1 = zipentry.getName();
-                            String[] ss = s1.split("/");
-                            String s2 = ss[ss.length - 1];
-                            if (!zipentry.isDirectory()) {
-                                if (s2.equals("fabric.mod.json")) {
-//                                  modName.clear();
-                                    fabric = true;
-                                    break;
-                                } else if (s2.endsWith(".class")) {
-                                    hasClass = true;
-                                }
-                            }
-                        }
-
-                        if (/* modName.isEmpty() && */ hasClass && !fabric) {
-                            modName.add(new DefaultModEntry(
-                                    name.replace(".zip", "").replace(".jar", ""),
-                                    remappedFile,
-                                    file
-                            ));
-                        }
-
-                        if (!modName.isEmpty()) {
-                            if (EXCLUDED.containsKey(modName.get(0).modName)) {
-                                FileUtils.removeEntriesFromZip(file.toPath(), EXCLUDED.get(modName.get(0).modName));
-                            }
-                        }
-                    } catch (IOException | URISyntaxException e) {
-                        throw new RuntimeException(e);
-                    }
+                    FileUtils.removeEntriesFromZip(modEntry.file.toPath(), EXCLUDED.get(modEntry.modId));
                 }
-
-                mods.addAll(modName);
             }
         }
 
